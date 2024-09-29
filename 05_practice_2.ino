f10092360f9ec38a758196d3b7dd2d772812c9b1
wlsdidgns2002 <wlsdidgns2002@gmail.com>
2024-09-29 23:19:21 +0900

    05_practice_2.ino
    GPIO를 이용한 LED제어 / 1초동안 점등후 이후 1초동안 5번 깜박임, 이후 꺼진상태로 무한루프

---
+++ 05_practice_2.ino
@@ -0,0 +1,21 @@
+const int ledPin = 7;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, LOW);
+  delay(1000); 
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(ledPin, LOW);
+    delay(100);
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+  }
+
+  digitalWrite(ledPin, HIGH);
+
+  while (true) {
+  }
+}
